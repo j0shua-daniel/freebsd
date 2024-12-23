ad4f1160e4effe808f213339c975e885af557a29
Joshua Daniel <joshua-daniel@mailfence.com>
2024-12-23 08:37:16 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ sysrc kld_list+=amdgpu
 echo  "%wheel ALL=(ALL) ALL" >> /usr/local/etc/sudoers.tmp
 sysrc powerd_enable="YES"
 sysrc powerd_flags="-a hiadaptive"
+echo '
+autoboot_delay="3"
+beastie_disable="YES"' >> /boot/loader.conf
