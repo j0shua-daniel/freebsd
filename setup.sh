17149f91973080e14181db21e779a2871747c568
Joshua Daniel <joshua-daniel@mailfence.com>
2025-01-03 08:40:19 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-pkg install drm-kmod sudo
-sysrc kld_list+=amdgpu
-sysrc powerd_enable="YES"
-sysrc powerd_flags="-a hiadaptive"
-echo '
-autoboot_delay="3"
-beastie_disable="YES"' >> /boot/loader.conf
