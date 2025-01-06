a4224611803deb3b7ad6a42421190fde5e07fb44
Joshua Daniel <joshua-daniel@mailfence.com>
2025-01-06 08:56:44 +0000

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pkg install sudo; sysrc powerd_enable="YES"; sysrc powerd_flags="-a hiadaptive"
+
+read -p "GPU: " answer
+
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+if [ $answer = nvidia ] ; then
+	pkg install nvidia-driver sudo; sysrc kld_list+=nvidia-modeset; break
+elif [ $answer = amd ] ; then
+	pkg install drm-kmod sudo;  sysrc kld_list+=amdgpu; break
+elif [ $answer = intel ] ; then
+	pkg install drm-kmod sudo; sysrc kld_list+=i915kms; break
+else
+	echo "GPU not found!"
+fi
