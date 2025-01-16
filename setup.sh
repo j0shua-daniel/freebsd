032b34b8d2f183c14393c9374a4b90c34e7f3603
Joshua Daniel <joshua-daniel@mailfence.com>
2025-01-16 17:51:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 
+if [ $(id -u) -ne 0 ]
+  then echo Please run this script as root or using doas!
+  exit
+fi
+
 pkg install doas; sysrc powerd_enable="YES"; sysrc powerd_flags="-a hiadaptive"
 
 echo "permit persist :wheel as root" >> /usr/local/etc/doas.conf
 
-read -p "GPU: [amd, nvidia, intel] " answer
+clear
+
+echo "CHOOSE A GPU:"
+echo "amd"
+
+read -p "GPU: " answer
 
 answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 
 if [ $answer = nvidia ] ; then
-	pkg install nvidia-driver sudo; sysrc kld_list+=nvidia-modeset; break
+	pkg install nvidia-driver; sysrc kld_list+=nvidia-modeset; break
 elif [ $answer = amd ] ; then
-	pkg install drm-kmod sudo;  sysrc kld_list+=amdgpu; break
+	pkg install drm-kmod;  sysrc kld_list+=amdgpu; break
 elif [ $answer = intel ] ; then
-	pkg install drm-kmod sudo; sysrc kld_list+=i915kms; break
+	pkg install drm-kmod; sysrc kld_list+=i915kms; break
+elif [ $answer = old-amd ] ; then
+	pkg install drm-kmod; sysrc kld_list+=radeonkms; break
 else
 	echo "GPU not found!"
 fi
